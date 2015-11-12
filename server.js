48b635441be6007631d28f58eaf664f35efc61cc
Samson Brock <SamsonLB@gmail.com>
2015-11-12 10:24:08 -0700

    Send connection status message to individual clients

--- server.js
+++ server.js
@@ -26,8 +26,12 @@ const io = socketIo(server);
 io.on('connection', function(socket) {
   console.log('A user has connected.');
   console.log(io.engine.clientsCount + ' user(s) now connected.');
+
   // Broadcast total connected user count to all users
   io.sockets.emit('usersConnected', io.engine.clientsCount + ' user(s) now connected.');
+
+  // Send message to current client (one socket = one client)
+  socket.emit('statusMessage', 'You have connected.');
 
   socket.on('disconnect', function() {
     console.log('A user has disconnected.');
